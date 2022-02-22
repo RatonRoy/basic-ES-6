e80a56ef111756ceb575dcd5fc9b3b19211ece20
RatonRoy <royraton18@gmail.com>
2022-02-22 14:11:36 +0600

    comment the functions

--- basic.js
+++ basic.js
@@ -2,3 +2,4 @@ const firstName = 'Raton';
 const lastName = 'Roy';
 const fullName = `Hi my name is ${firstName} ${lastName}`;
 console.log(fullName);
+/* `` this simbole name is  backtik and another time it call the templete string simbole  */
